127510fd2c1b05013da630ab78f5cf82ab712a62
cKnei <notknei@gmail.com>
2025-02-01 14:56:46 -0500

    Finished HotPotato.java

--- Labs/Completed Lab/Lab 3 - Hot Potato/src/hotpotato/HotPotato.java
+++ Labs/Completed Lab/Lab 3 - Hot Potato/src/hotpotato/HotPotato.java
@@ -13,17 +13,25 @@ import java.util.Random;
  * and then plays the game to completion.
  *
  * @author RIT CS
- * @author YOUR NAME HERE
+ * @author Knei
  */
 public class HotPotato {
-    /** the maximum number of players */
+    /**
+     * The maximum number of players
+     */
     private final static int MAX_PLAYERS = 20;
-    /** the maximum number of passes that can be made in a single round */
+    /**
+     * The maximum number of passes that can be made in a single round
+     */
     private final static int MAX_PASSES = 10;
-    /** use this if not seeding the random number generator for truly random game play */
+    /**
+     * Use this if not seeding the random number generator for truly random game play
+     */
     private final static int NO_SEED = 0;
 
-    /** A collection of 20 players */
+    /**
+     * A collection of 20 players
+     */
     private final static List<Player> players =
             Arrays.asList(
                     new Player("Alakazam", 0),
@@ -46,31 +54,40 @@ public class HotPotato {
                     new Player("Raichu", 17),
                     new Player("Squirtle", 18),
                     new Player("Typhlosion", 19));
-    /** the random number generator */
+    /**
+     * The random number generator
+     */
     private Random random;
-    /** the current round number, starting at 1 */
+    /**
+     * The current round number, starting at 1
+     */
     private int round;
-    /** the circle of players */
+    /**
+     * The circle of players
+     */
     private CircularList<Player> circle;
 
     /**
      * Initialize the game by creating the circle and adding players in order
      * from the above list of players.  Also creates the random number generator
      * with a seed (or not).
-     * @see CircularListLinkedList#CircularListLinkedList()
-     * @see CircularListLinkedList#append(Object)
+     *
      * @param size the size of the circle
      * @param seed the seed of the random number generator
+     * @see CircularListLinkedList#CircularListLinkedList()
+     * @see CircularListLinkedList#append(Object)
      */
     public HotPotato(int size, int seed) {
         // create the initial circle as a circular list linked list as empty
-        // TODO
+        this.circle = new CircularListLinkedList<>();
 
         // add the players to the circle from front to back using the provided players list
-        // TODO
+        for ( int i = 0; i < size; i++ ) {
+            this.circle.append(players.get(i));
+        }
 
         // create random number generator based on seed or no seed
-        if (seed == NO_SEED) {
+        if ( seed == NO_SEED ) {
             this.random = new Random();
         } else {
             this.random = new Random(seed);
@@ -81,18 +98,20 @@ public class HotPotato {
     }
 
     /**
-     * Generate a random number from 0 to MAX_PASSES, inclusive.  This is
+     * Generate a random number from 0 to MAX_PASSES, inclusive. This is
      * used to determine for the current round how many times to pass the
      * hot potato.
+     *
      * @return a random number between 0 and MAX_PASSES
      */
     public int getRandomNumPasses() {
-        return this.random.nextInt(MAX_PASSES+1);
+        return this.random.nextInt(MAX_PASSES + 1);
     }
 
     /**
      * Display to standard output the round, number of players still active,
      * and the players in the circle from front to back.
+     *
      * @see CircularListLinkedList#size()
      * @see CircularListLinkedList#toString()
      */
@@ -120,33 +139,62 @@ public class HotPotato {
      * @see CircularListLinkedList#removeBackward()
      */
     public void playGame() {
-        // TODO
+        while ( this.circle.size() > 1 ) {
+            this.display();
+
+            int passes = this.getRandomNumPasses();
+
+            if ( this.round % 2 == 1 ) {
+                System.out.println("Passing potato " + passes + " times RIGHT:");
+                for ( ; passes > 0; passes-- ) {
+                    System.out.println("\t:" + this.circle.get() + " has potato and passing RIGHT");
+                    this.circle.forward();
+                }
+                System.out.println(this.circle.removeForward() + " lost!");
+            } else {
+                System.out.println("Passing potato " + passes + " times LEFT:");
+                for ( ; passes > 0; passes-- ) {
+                    System.out.println("\t:" + this.circle.get() + " has potato and passing LEFT");
+                    this.circle.backward();
+                }
+                System.out.println(this.circle.removeBackward() + " lost!");
+            }
+
+            this.round++;
+            System.out.println();
+        }
+
+        this.display();
+        System.out.println(this.circle.get() + " wins!");
     }
 
     /**
      * The main method expects the number of players and the seed on the command line.
      * If everything is in order the game is created and played.
+     *
      * @param args command line arguments containing number of players and random number generator seed.
      * @see HotPotato#HotPotato(int, int)
      * @see HotPotato#playGame()
      */
     public static void main(String[] args) {
-        if (args.length != 2) {
+        if ( args.length != 2 ) {
             System.out.println("Usage: java HotPotato #players seed");
-        } else {
-            int size = Integer.parseInt(args[0]);
-            int seed = Integer.parseInt(args[1]);
-
-            System.out.println("Welcome to Pokemon Hot Potato v1.0 :)");
-            System.out.println("Number of players: " + size + ", Seed: " + ((seed != NO_SEED) ? seed : "NO SEED!"));
-            System.out.println();
-
-            if (size < 0 || size > MAX_PLAYERS) {
-                System.out.println("Number of players must be between 1 and " + MAX_PLAYERS);
-            } else {
-                HotPotato game = new HotPotato(size, seed);
-                game.playGame();
-            }
+            return;
         }
+
+        int size = Integer.parseInt(args[0]);
+        int seed = Integer.parseInt(args[1]);
+
+        System.out.println("Welcome to Pokemon Hot Potato v1.0 :)");
+        System.out.println("Number of players: " + size + ", Seed: " + ((seed != NO_SEED) ? seed : "NO SEED!"));
+        System.out.println();
+
+        if ( size < 0 || size > MAX_PLAYERS ) {
+            System.out.println("Number of players must be between 1 and " + MAX_PLAYERS);
+            return;
+        }
+
+        HotPotato game = new HotPotato(size, seed);
+        game.playGame();
     }
 }
