ca9cbe6b54804d873394a29b293be2181fcf1698
cKnei <notknei@gmail.com>
2025-02-01 15:30:20 -0500

    Genericized DLNode.java

--- Labs/Completed Lab/Lab 3 - Hot Potato/src/rit/stu/DLNode.java
+++ Labs/Completed Lab/Lab 3 - Hot Potato/src/rit/stu/DLNode.java
@@ -1,36 +1,44 @@
 package rit.stu;
 
 /**
- * A doubly linked node that holds a String as its data type.
+ * A generic doubly linked node that holds a type {@code T} as its data.
  *
  * @author RIT CS
- * @author YOUR NAME HERE
+ * @author Knei
  */
-public class DLNode {
-    /** User data */
-    private String data;
+public class DLNode<T> {
+    /**
+     * User data
+     */
+    private T data;
 
-    /** Previous node link */
-    private DLNode prev;
+    /**
+     * Previous node link
+     */
+    private DLNode<T> prev;
 
-    /** Next node link */
-    private DLNode next;
+    /**
+     * Next node link
+     */
+    private DLNode<T> next;
 
     /**
      * Create a new node with no predecessor or successor.
+     *
      * @param data the user data to be stored
      */
-    public DLNode(String data) {
+    public DLNode(T data) {
         this(data, null, null);
     }
 
     /**
      * Construct a new node with pointers to the previous and next node.
+     *
      * @param data The user data to be stored
      * @param prev The link to the previous node (null if none)
      * @param next The link to the next node (null if none)
      */
-    public DLNode(String data, DLNode prev, DLNode next) {
+    public DLNode(T data, DLNode<T> prev, DLNode<T> next) {
         this.data = data;
         this.prev = prev;
         this.next = next;
@@ -38,49 +46,55 @@ public class DLNode {
 
     /**
      * Get the predecessor node.
+     *
      * @return the previous node (null if none)
      */
-    public DLNode getPrev() {
+    public DLNode<T> getPrev() {
         return this.prev;
     }
 
     /**
-     * Get the successor node.
-     * @return the next node (null if none)
-     */
-    public DLNode getNext() {
-        return this.next;
-    }
-
-    /**
-     * Get the Node's data.
-     * @return the user data
-     */
-    public String getData() {
-        return this.data;
-    }
-
-    /**
      * Change the node's successor.
+     *
      * @param prev the node's new next link
      */
-    public void setPrev(DLNode prev) {
+    public void setPrev(DLNode<T> prev) {
         this.prev = prev;
     }
 
     /**
+     * Get the successor node.
+     *
+     * @return the next node (null if none)
+     */
+    public DLNode<T> getNext() {
+        return this.next;
+    }
+
+    /**
      * Change the node's successor.
+     *
      * @param next the node's new next link
      */
-    public void setNext(DLNode next) {
+    public void setNext(DLNode<T> next) {
         this.next = next;
     }
 
     /**
+     * Get the Node's data.
+     *
+     * @return the user data
+     */
+    public T getData() {
+        return this.data;
+    }
+
+    /**
      * Change the node's data.
+     *
      * @param data the node's new data
      */
-    public void setData(String data) {
+    public void setData(T data) {
         this.data = data;
     }
 
@@ -100,6 +114,7 @@ public class DLNode {
      *         ZZZ: The data element of the next node.  If no node, "null".
      *     </li>
      * </ul>
+     *
      * @return the string described above
      */
     @Override
