a3d92e734de9c676f35140431f7b3687e64b522a
cKnei <notknei@gmail.com>
2025-02-15 12:41:44 -0500

    Initial Import of Lab 5

---
+++ Labs/Completed Lab/Lab 5 - Arnolds/src/simulation/Arnolds.java
@@ -0,0 +1,11 @@
+package simulation;
+
+import java.io.FileNotFoundException;
+
+public class Arnolds {
+    public Arnolds(String filename) throws FileNotFoundException {
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+    }
+}
